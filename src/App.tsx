ecf7bb8e9408b0252f218c19be075d4ad2c1bc8d
suh124 <suhtjddmlgus124@gmail.com>
2025-05-22 16:58:39 +0900

    FEAT: First Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export default function App() {
+    return (
+        <Container className="my-3">
+            Hello, world!
+        </Container>
+    );
+}
